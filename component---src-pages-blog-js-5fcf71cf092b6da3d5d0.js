a1851d829f9aa3be59031c52ff065b2c8861e7a5
oksourav <oksourav@users.noreply.github.com>
2024-12-04 12:25:35 +0000

    deploy: 0b9ad3161205f6a707d0a421f88e493452eb32ee

---
+++ component---src-pages-blog-js-5fcf71cf092b6da3d5d0.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdigifact_website=self.webpackChunkdigifact_website||[]).push([[711],{4151:function(e,t,a){a.r(t);var n=a(6540),c=a(2600);t.default=()=>n.createElement(c.A,null,n.createElement("main",{className:"page-container"},n.createElement("h1",{className:"text-center my-20 text-3xl"},"Coming Soon!")))}}]);
+//# sourceMappingURL=component---src-pages-blog-js-5fcf71cf092b6da3d5d0.js.map
